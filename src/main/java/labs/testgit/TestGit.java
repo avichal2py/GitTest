ccd961b442ea08c508ffeac701ff93195dc0cb36
Aayush Kumar <avichal0722@gmail.com>
2024-10-02 14:18:06 +1200

    new commit

---
+++ src/main/java/labs/testgit/TestGit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package labs.testgit;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class TestGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
